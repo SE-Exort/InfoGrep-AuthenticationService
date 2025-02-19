f279b2804ab51724d607e2c511530389b16da93c
TyroneHe-0926 <tyrone99926@gmail.com>
2025-02-18 22:57:04 -0500

    chore: adding cert verify for postgres connection in cluster

--- db.py
+++ db.py
@@ -11,8 +11,21 @@ db_name = os.environ.get("PG_DATABASE_NAME", "postgres")
 
 DATABASE_URL = f"postgresql://{db_user}:{db_password}@{db_host}/{db_name}"
 
+engine = None
+
 # Create SQLAlchemy engine
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+if os.environ.get("PG_VERIFY_CERT") == "true":
+    ca_cert_path = os.environ["PG_CA_CERT_PATH"]
+    client_cert_path = os.environ["PG_TLS_CERT_PATH"]
+    client_key_path = os.environ["PG_TLS_KEY_PATH"]
+    ssl_args = {
+        'sslrootcert':ca_cert_path,
+        'sslcert':client_cert_path,
+        'sslkey':client_key_path
+    }
+    engine = create_engine(DATABASE_URL, pool_pre_ping=True, connect_args=ssl_args)
+else:
+    engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
 def get_db():
     db = Session(bind=engine)
